96132846cf051dbfe2f58bce87b20fc65f921e4d
Mike Hodnick <mike.hodnick@gmail.com>
2012-01-06 08:50:11 -0600

    added lfo wave form

--- ota-ui.js
+++ ota-ui.js
@@ -11,7 +11,7 @@ $(document).ready(function () {
     hideControls();
     $('#container').mouseenter(function () { showControls(); });
     $('#container').mouseleave(function () { hideControls(); });
-    setUpSliders();
+    initControls();
     buildAudio();
 });
 
@@ -23,7 +23,7 @@ function showControls() {
     $('#controlOverlay').css('visibility', 'visible');
 }
 
-function setUpSliders() {
+function initControls() {
 
     $('#osc1HzSlider').slider({
         orientation: 'vertical',
@@ -31,7 +31,7 @@ function setUpSliders() {
         max: 1000,
         value: 440,
         change: function (event, ui) { oscillator1.frequency = ui.value; }
-	, slide: function(event, ui) { oscillator1.frequency = ui.value; }
+	, slide: function (event, ui) { oscillator1.frequency = ui.value; }
     });
 
     $('#lfo1HzSlider').slider({
@@ -40,8 +40,8 @@ function setUpSliders() {
         max: 100,
         value: 0,
         change: function (event, ui) { lfo1.frequency = ui.value; },
-	slide: function(event, ui) { lfo1.frequency = ui.value; },
-	step: .1
+        slide: function (event, ui) { lfo1.frequency = ui.value; },
+        step: .1
     });
 
     $('#lfo1AmountSlider').slider({
@@ -49,9 +49,9 @@ function setUpSliders() {
         min: 0,
         max: 1,
         value: 0,
-        change: function (event, ui) { lfo1Amount= ui.value; },
-	slide: function(event, ui) { lfo1Amount= ui.value; },
-	step: .01
+        change: function (event, ui) { lfo1Amount = ui.value; },
+        slide: function (event, ui) { lfo1Amount = ui.value; },
+        step: .01
     });
 
 
@@ -61,7 +61,7 @@ function setUpSliders() {
         max: 1000,
         value: 440,
         change: function (event, ui) { oscillator2.frequency = ui.value; },
-	    slide: function(event, ui) {oscillator2.frequency = ui.value; }
+        slide: function (event, ui) { oscillator2.frequency = ui.value; }
     });
 
     $('#lfo2HzSlider').slider({
@@ -70,50 +70,61 @@ function setUpSliders() {
         max: 100,
         value: 0,
         change: function (event, ui) { lfo2.frequency = ui.value; },
-	    slide: function(event, ui){ lfo2.frequency = ui.value; }
+        slide: function (event, ui) { lfo2.frequency = ui.value; }
     });
 
     $('#lfo2AmountSlider').slider({
-	    orientation: 'vertical',
-	    min: 0,
-	    max: 1,
-	    value: 0,
-	    change: function(event, ui) { lfo2Amount = ui.value; },
-	    slide: function(event, ui) { lfo2Amount = ui.value; },
-	    step: .01
+        orientation: 'vertical',
+        min: 0,
+        max: 1,
+        value: 0,
+        change: function (event, ui) { lfo2Amount = ui.value; },
+        slide: function (event, ui) { lfo2Amount = ui.value; },
+        step: .01
     });
 
     $('#volumeSlider').slider({
-	    orientation: 'vertical',
-	    min: 0,
-	    max: 1,
-	    value: .5,
-	    step: .01,
-	    slide: function(event, ui) { volume = ui.value; },
-	    change: function(event, ui) { volume = ui.value; }
+        orientation: 'vertical',
+        min: 0,
+        max: 1,
+        value: .5,
+        step: .01,
+        slide: function (event, ui) { volume = ui.value; },
+        change: function (event, ui) { volume = ui.value; }
     });
 
-    $('#waveFormButton').click(function(){
-	    var waveShape = oscillator1.waveShape;
-	switch(waveShape){
-		case 'sine':
-			waveShape = 'square';
-			break;
-		case 'square':
-			waveShape = 'triangle';
-			break;
-		case 'triangle':
-			waveShape = 'sawtooth';
-			break;
-		case 'sawtooth':
-			waveShape = 'sine';
-			break;
-	}
-	oscillator1.waveShape = waveShape;
-	oscillator2.waveShape = waveShape;
-	$('#waveFormButton').text(waveShape);
+    $('#waveFormButton').click(function () {
+        oscillator1.waveShape = getNextWaveShape(oscillator1.waveShape);
+        oscillator2.waveShape = oscillator1.waveShape;
+        $('#waveFormButton').text(oscillator1.waveShape);
+
     });
 
+    $('#lfo1WaveFormButton').click(function () {
+        lfo1.waveShape = getNextWaveShape(lfo1.waveShape);
+        $('#lfo1WaveFormButton').text(lfo1.waveShape);
+
+    });
+
+    $('#lfo2WaveFormButton').click(function () {
+        lfo2.waveShape = getNextWaveShape(lfo2.waveShape);
+        $('#lfo2WaveFormButton').text(lfo2.waveShape);
+    });
+}
+
+function getNextWaveShape(shape) {
+    switch (shape) {
+        case 'sine':
+            return 'square';
+        case 'square':
+            return 'triangle';
+        case 'triangle':
+            return 'sawtooth';
+        case 'sawtooth':
+            return 'invSawtooth';
+        case 'invSawtooth':
+            return 'sine';
+    }
 }
 
 function updateCenter() {
@@ -134,12 +145,12 @@ function buildAudio() {
 
 function audioCallback(buffer, channelCount) {
     var l = buffer.length, current;
-    
+
     for (current = 0; current < l; current += channelCount) {
         lfo1.generate();
         lfo2.generate();
-	oscillator1.fm = lfo1.getMix() * lfo1Amount;
-	oscillator2.fm = lfo2.getMix() * lfo2Amount;
+        oscillator1.fm = lfo1.getMix() * lfo1Amount;
+        oscillator2.fm = lfo2.getMix() * lfo2Amount;
         oscillator1.generate();
         oscillator2.generate();
         buffer[current] = oscillator1.getMix() * volume;
